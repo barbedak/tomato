3c16f9ad7f8f45849e12054e292e667dcd6be071
Andrey Kurochkin <barbedak@gmail.com>
2025-12-04 16:00:23 +0300

    hmwork 25.1

--- tomato/app/Http/Controllers/Client/PostController.php
+++ tomato/app/Http/Controllers/Client/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Client\Post\ShowPostRequest;
 use App\Http\Requests\Client\Post\StoreCommentRequest;
 use App\Http\Resources\Comment\CommentResource;
 use App\Http\Resources\Post\PostResource;
@@ -17,13 +18,21 @@ class PostController extends Controller
         return inertia('Client/Post/Index', compact('posts'));
     }
 
-    public function show(Post $post)
+    public function show(Post $post, ShowPostRequest $request)
     {
-        $comments=CommentResource::collection($post->comments)->resolve();
+//        $comments=CommentResource::collection($post->comments)->resolve(); //->resolve() возвращает массив
+        $data = $request->validationData();
+        $comments = CommentResource::collection(
+            $post->comments()->paginate(5, '*', 'page', $data['page'])
+        );
+
 //        $post = $post->toResource();
 //        или
 
         $post = PostResource::make($post)->resolve();
+        if ($request->wantsJson()) {
+            return $comments;
+        }
         return inertia('Client/Post/Show', compact('post', 'comments'));
     }
 
@@ -34,6 +43,7 @@ class PostController extends Controller
             'message' => 'Post deleted successfully'
         ], Response::HTTP_OK);
     }
+
     public function toggleLike(Post $post)
     {
         auth()->user()->profile->likedPosts()->toggle($post->id); //меняем в базе, для актуализации объекта нужно вызвать fresh()
--- tomato/resources/js/Pages/Client/Post/Show.vue
+++ tomato/resources/js/Pages/Client/Post/Show.vue
@@ -4,13 +4,18 @@
             <ItemPost :post="post"></ItemPost>
         </div>
         <div>
-            <div v-if="comments.length > 0" class="mb-4 border border-gray-200 bg-white p-4">
+            <div v-if="commentsData.data.length > 0" class="mb-4 border border-gray-200 bg-white p-4">
                 <div class="mb-4">
                     <h3>Comments</h3>
                 </div>
                 <div>
-                    <ItemComment v-for="comment in comments" :comment="comment"></ItemComment>
+                    <ItemComment v-for="comment in commentsData.data" :comment="comment"></ItemComment>
                 </div>
+            </div>
+            <div v-if="is_show">
+                <a href="#" @click.prevent="getComments"
+                   class="block py-4 bg-sky-700 border-sky-800 text-white text-center">We need more
+                    comments...</a>
             </div>
             <div class="border border-gray-200 bg-white p-4">
                 <div class="mb-4">
@@ -39,10 +44,10 @@ export default {
             type: Object,
             required: true,
         },
-         comments: {
+        comments: {
             type: Array,
-             required: false
-         }
+            required: false
+        }
     },
 
     components: {
@@ -50,20 +55,34 @@ export default {
         ItemPost
     },
 
-    data(){
+    data() {
         return {
             comment: {
                 body: '',
-            }
+            },
+            page: 1,
+            commentsData: this.comments,
+            is_show: this.comments.meta.to < this.comments.meta.total
         }
     },
 
     methods: {
-        storeComment(){
+        storeComment() {
             axios.post(route('client.posts.comments.store', this.post.id), this.comment)
                 .then(res => {
                     this.comments.push(res.data)
                     this.comment.body = ''
+                })
+        },
+        getComments() {
+            axios.get(route('client.posts.show', this.post), {
+                params: {
+                    page: ++this.page
+                }
+            })
+                .then(res => {
+                    this.is_show = res.data.meta.to < res.data.meta.total
+                    this.commentsData.data = [...this.commentsData.data, ...res.data.data]
                 })
         }
     }
